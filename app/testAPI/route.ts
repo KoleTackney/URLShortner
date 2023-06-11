da286daee52b6d534cd91ee3309b2855e3d69f1c
Kole Tackney <45576587+KoleTackney@users.noreply.github.com>
2023-06-11 19:01:29 -0400

    add test route

--- app/testAPI/route.ts
+++ app/testAPI/route.ts
@@ -1,7 +1,5 @@
 import { NextResponse } from 'next/server'
 
-export async function GET() {
-    console.log("Hello world");
-
-    return "hi";
+export async function GET(request: Request) {
+    console.log("Hello, basic get");
 }
